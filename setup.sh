d8c8984bb4bc19feac067e9d8c1dfa952c03b5cd
Marcus Fermino <marcusfermino.linux@gmail.com>
2024-11-22 12:26:50 -0300

    Inicialização

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Executa as opções relacionadas com o Vagrant
+vagrant() {
+    curl 
+}
+
+
+# Verificar qual é a distribuição Linux
+# Carregar o script de instalaçãorelaaionado com aquela distro
