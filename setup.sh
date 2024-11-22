9af5cebb345399d88ec2d4979ff9bf733f88b622
Marcus Fermino <marcusfermino.linux@gmail.com>
2024-11-22 15:08:10 -0300

    removed setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,1 @@
 #!/usr/bin/env bash
-
-# Executa as opções relacionadas com o Vagrant
-vagrant() {
-    curl https://raw.githubusercontent.com/ferminolinux/setup/refs/heads/main/scripts/vagrant/vagrant.sh | bash
-}
-
-
-# Verificar qual é a distribuição Linux
-# Carregar o script de instalaçãorelaaionado com aquela distro
