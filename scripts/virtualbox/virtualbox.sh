ba74813da196dbe11d660b5020feb845b757fc92
Marcus Fermino <marcusfermino.linux@gmail.com>
2024-11-22 13:36:04 -0300

    virtualbox.sh

--- scripts/virtualbox/virtualbox.sh
+++ scripts/virtualbox/virtualbox.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 check_os_family() {
-    os_family="$(curl https://raw.githubusercontent.com/ferminolinux/setup/refs/heads/main/scripts/utils/check_os_family.sh | bash)"
+    os_family="$(curl -qs https://raw.githubusercontent.com/ferminolinux/setup/refs/heads/main/scripts/utils/check_os_family.sh | bash)"
     echo $os_family
 }
 
 debian() {
     # $1 virtualbox version
 
-    version_codename=$(cat /etc/os-release | grep "VERSION_CODENAME")
+    version_codename=$(cat /etc/os-release | grep "VERSION_CODENAME" | cut -d '=' -f2)
     repo="deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian ${version_codename} contrib"
     vb_version="$1"
 
@@ -16,17 +16,17 @@ debian() {
 
     wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
 
-    sudo apt-get update -y
-    sudo apt-get install "virtualbox-${vb_version}"
+    apt-get update -y
+    apt-get install "virtualbox-${vb_version}"
 }
 
 declare vb_version 
 
 [[ $# -eq 0 ]] && vb_version="6.0" || vb_version="${1}"
 
-case "${os_family}" in
+case "$(check_os_family)" in
     debian)
-        debian() $vb_version
+        debian $vb_version
         ;; 
 esac
 
