a66d656e97dfb7ba301e31d37ff8854e98bd5fa8
Marcus Fermino <marcusfermino.linux@gmail.com>
2024-11-22 14:17:26 -0300

    virtualbox.sh

--- scripts/virtualbox/virtualbox.sh
+++ scripts/virtualbox/virtualbox.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -r SCRIPT_NAME="${0%.*}"
+declare -r SCRIPT_NAME="echo ${0%.*} | basename"
 
 check_os_family() {
     os_family="$(curl -qs https://raw.githubusercontent.com/ferminolinux/setup/refs/heads/main/scripts/utils/check_os_family.sh | bash)"
@@ -10,11 +10,11 @@ check_os_family() {
 debian() {
     # $1 virtualbox version
 
-    version_codename=$(cat /etc/os-release | grep "VERSION_CODENAME" | cut -d '=' -f2)
-    repo="deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian ${version_codename} contrib"
-    vb_version="$1"
+    declare version_codename=$(cat /etc/os-release | grep "VERSION_CODENAME" | cut -d '=' -f2)
+    declare repo="deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian ${version_codename} contrib"
+    declare vb_version="$1"
 
-    echo "Configurando o repositório"
+    echo "Configurando o repositório..."
 
     echo "${repo}" | tee "/etc/apt/sources.list.d/virtualbox-${version_codename}.list"
     wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
@@ -28,7 +28,7 @@ debian() {
     wait
 
     echo "Configurando os modulos do kernel"
-    vboxconfig &> " /var/log/${SCRIPT_NAME}/vboxconfig.log" &
+    vboxconfig &> "/var/log/${SCRIPT_NAME}/vboxconfig.log" &
     wait
 }
 
@@ -36,7 +36,7 @@ declare vb_version
 
 [[ $# -eq 0 ]] && vb_version="7.0" || vb_version="${1}"
 
-mkdir "/var/log/${SCRIPT_NAME}"
+mkdir -p "/var/log/${SCRIPT_NAME}"
 
 case "$(check_os_family)" in
     debian)
