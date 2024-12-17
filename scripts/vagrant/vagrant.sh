17c6940668c6c8d064dbd4f39316638f308b6868
Marcus Fermino <marcusfermino.linux@gmail.com>
2024-12-17 19:34:27 -0300

    Adicionando jinja

--- scripts/vagrant/vagrant.sh
+++ scripts/vagrant/vagrant.sh
@@ -1,2 +1,26 @@
 #!/usr/bin/env bash
 
+debian() {
+    apt-get install -y python3-pip &
+    wait
+    pip3 install pipx &
+    wait
+    pipx install ansible &
+    wait
+    pipx install ansible-core &
+    wait
+    pipx ensurepath
+    ansible --version
+}
+
+check_os_family() {
+    apt-get install -y curl
+    os_family="$(curl -qs https://raw.githubusercontent.com/ferminolinux/setup/refs/heads/main/scripts/utils/check_os_family.sh | bash)"
+    echo $os_family
+}
+
+case "$(check_os_family)" in 
+    debian)
+        debian 
+        ;;
+esac
